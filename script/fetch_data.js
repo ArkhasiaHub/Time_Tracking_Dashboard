f6b316bb9031b84e37288447a841426f9da9f822
Arkhasia <146063272+ArkhasiaHub@users.noreply.github.com>
2025-10-03 11:16:44 +0200

    Add Javascript logic
    feat: add JavaScript logic to allow users to switch between daily, weekly, and monthly timeframes.

---
+++ script/fetch_data.js
@@ -0,0 +1,42 @@
+const url = '../data.json';
+
+function getReponseJson(response) {
+    if (!response.ok) {
+        throw new Error(`HTTP error: ${response.status}`);
+    }
+    return response.json();
+}
+
+function getStat(data, timeframe) {
+    let title = data['title'];
+    let {current, previous} = data['timeframes'][timeframe];
+    return [title, current, previous];
+}
+
+function updateStat(stat) {
+    const [id, current, previous] = stat;
+    const component = document.getElementById(id.toLowerCase().replace(' ', '-'));
+    const spanTimeFrame = component.querySelectorAll('span');
+    spanTimeFrame[0].textContent = `${current}hrs`;
+    spanTimeFrame[1].textContent = `Previous - ${previous}hrs`;
+}
+
+function parseData(data, timeFrame) {
+    data.forEach((item) => {
+        updateStat(getStat(item, timeFrame));
+    });
+}
+
+export async function fetchData(timeframe) {
+    try {
+        const promise = await fetch(url);
+        const json = await promise.json();
+        parseData(json, timeframe);
+    } catch (error) {
+        console.error(`Error occured: ${error}`);
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    fetchData('daily');
+});
---
+++ script/handle_buttons.js
@@ -0,0 +1,33 @@
+import { fetchData } from "./fetch_data.js";
+
+const timeframeList = document.getElementById('timeframe-list');
+const buttons = timeframeList.querySelectorAll('.profile-report button');
+
+function removeCurrentTimeFrame() {
+    const timeFrame = document.querySelector('.selected-timeframe');
+    timeFrame.classList.remove('selected-timeframe');
+}
+
+function addSelectedTimeFrame(i) {
+    const timeFrame = document.querySelectorAll('.profile-report li');
+    timeFrame[i].classList.add('selected-timeframe');
+}
+
+function getSelectedLiChild(id) {
+    switch (id) {
+        case 'button-daily': return [0, 'daily'];
+        case 'button-weekly': return [1, 'weekly'];
+        case 'button-monthly': return [2, 'monthly'];
+    }
+}
+
+async function handle_button(e) {
+    const [selectedLiChild, timeframe] = getSelectedLiChild(e.target.id);
+    removeCurrentTimeFrame();
+    addSelectedTimeFrame(selectedLiChild);
+    await fetchData(timeframe);
+}
+
+buttons.forEach(function(button) { 
+    button.addEventListener('click', handle_button); 
+});
